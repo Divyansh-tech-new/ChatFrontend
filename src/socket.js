09ae4a09e1973c8443293d6a0903567989333feb
Divyansh Negi <b24122@students.iitmandi.ac.in>
2025-06-26 16:15:49 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000/', {
-    transports: ['websocket'], // Use WebSocket protocol
-    withCredentials: true, // Allow credentials
+const socket = io(import.meta.env.VITE_HOST, {
+    transports: ['websocket'],
+    withCredentials: true,
 });
 
 export default socket;
